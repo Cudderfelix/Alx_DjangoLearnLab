9690c6d6b4e21498dfde6256d85059a995f1a645
Cudderfelix <ifeachochukafelix@gmail.com>
2025-10-06 19:54:34 +0000

    Add migrations for api app

---
+++ advanced-api-project/api/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Author
+from .serializers import AuthorSerializer
+
+@api_view(['GET'])
+def list_authors(request):
+    """Returns a list of authors with their books."""
+    authors = Author.objects.all()
+    serializer = AuthorSerializer(authors, many=True)
+    return Response(serializer.data)
