446860ab322f2c7935f4bf18e3e69e5f449fa758
Cudderfelix <ifeachochukafelix@gmail.com>
2025-10-13 00:43:20 +0100

    made modification to sqlite db and postgress db

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
-from pathlib import Path
 import os
+from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,6 +78,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': 'django_blog_db',
+       #'USER': 'alx_user',
+       # 'PASSWORD': 'alx_client',
+       # 'HOST': '127.0.0.1',
+       # 'PORT': '5432',
+
     }
 }
 
