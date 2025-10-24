cb9889bee8d3169458daa032b10b6767c314de33
Cudderfelix <ifeachochukafelix@gmail.com>
2025-10-24 12:55:46 +0100

    modified models.py and query.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -28,7 +28,7 @@ def get_librarian_for_library(library_name):
     """
     Retrieve the librarian for a library.
     """
-    library = Library.objects.get(name=library_name)
+    library = Librarian.objects.get(name=library_name)
     try:
         librarian = library.librarian
         print(f"Librarian for '{library_name}': {librarian}")
