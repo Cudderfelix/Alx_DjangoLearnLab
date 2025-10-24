3f62081b893b33a61d8598b9fc3f6b229ae91851
Cudderfelix <ifeachochukafelix@gmail.com>
2025-10-24 09:50:02 +0100

    modified admin.py to include filters and search capabilites

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -4,3 +4,16 @@ from django.contrib import admin
 from .models import Book
 
 admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    # 1. Columns that appear in the list view
+    list_display = ("title", "author", "publication_year")
+
+    # 2. Filters that appear on the right-hand sidebar
+    list_filter = ("author", "publication_year")
+
+    # 3. Search box - searches title and author fields
+    search_fields = ("title", "author")
+
+    # (optional) make the title column a clickable link to the change page
+    list_display_links = ("title",)
