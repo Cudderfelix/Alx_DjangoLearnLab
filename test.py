7ec57c5aaa7a890bfdce6116022b8364ff5f411e
Cudderfelix <ifeachochukafelix@gmail.com>
2025-10-24 09:39:35 +0100

    admin.py config creation

---
+++ test.py
@@ -0,0 +1,36 @@
+name = input ("Hello! Please input your name: ")
+print ("Welcome", 'my name is :', name)
+
+x, y = input ("Enter two numbers seperated by a comma:").split()
+print ("The age of the girl is: ", y)
+print ("The age of the boy is: ", x)
+
+
+# Taking 3 inputs from user using single input() statement
+a, b, c = input("Enter three values separated by comma: ").split()
+print ("The machine age is: ", a)
+print ("The car year is: ", b)
+print ("The bike year is: ", c)
+
+
+a = "Hello my people"
+b = 10
+c = 12.33
+d = ("Geeks", "for", "Geeks")
+e = ["Geeks", "for", "Geeks"]
+f = {"Geeks": 1, "for": 2, "Geeks": 3}
+
+print (a)
+print (b)
+print (c)
+print (d)
+print (e)
+print (f)
+
+
+print (type(a))
+print (type(b))
+print (type(c))
+print (type(d))
+print (type(e))
+print (type(f))
