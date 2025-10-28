cf9982b9cec408f0e90708546b9a5afc89a6c4ba
Cudderfelix <ifeachochukafelix@gmail.com>
2025-10-28 13:07:57 +0100

    added views and relationship

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@
 # relationship_app/views.py
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from . models import Book, Library
 
 
 def list_books(request):
