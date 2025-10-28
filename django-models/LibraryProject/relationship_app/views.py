bbbd7f4de1adedaa55c4834dda4ed3694a3be9c4
Cudderfelix <ifeachochukafelix@gmail.com>
2025-10-28 12:49:33 +0100

    added views and relationship

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,7 +9,7 @@ from .models import Book, Library
 # 1. Function-based view – list all books (plain HTML)
 # -------------------------------------------------
 def list_books(request):
-    books = Book.objects.select_related('author').all()
+    books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
