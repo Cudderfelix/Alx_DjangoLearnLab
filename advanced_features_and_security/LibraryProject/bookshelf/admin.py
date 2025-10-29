dc50675873ec81349e25b190aac2f6efe431b560
Cudderfelix <ifeachochukafelix@gmail.com>
2025-10-29 08:14:51 +0100

    reconfigured advanced_features_security_lib to fix error1.1

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -17,3 +17,35 @@ class BookAdmin(admin.ModelAdmin):
 
     # (optional) make the title column a clickable link to the change page
     list_display_links = ("title",)
+
+# bookshelf/admin.py
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, UserProfile, Book, Library, Author
+
+
+# --- CUSTOM USER ADMIN ---
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'date_of_birth', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'date_of_birth', 'profile_photo'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+# --- REGISTER MODELS ---
+admin.site.register(CustomUser, CustomUserAdmin)   # REQUIRED LINE
+admin.site.register(UserProfile)
+admin.site.register(Book)
+admin.site.register(Library)
+admin.site.register(Author)
